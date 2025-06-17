c29fa182362dbf8c5b8b120dc7f3f1b9dde34540
nithin-sivakumar <nithinsgayathri@gmail.com>
2025-06-17 16:07:33 +0530

    fix: add cors middleware to whitelist requests from known origins

--- server/main.py
+++ server/main.py
@@ -9,9 +9,23 @@ import uuid
 import os
 import datetime
 from utils.vector_service import collection
+from fastapi.middleware.cors import CORSMiddleware
+from utils.vector_service import vector_store
 
 app = FastAPI()
-from utils.vector_service import vector_store
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
